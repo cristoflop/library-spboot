caf4dae0158afd4013ff2f9ad06e7f962dc505ac
cristoflop <cristofer.lc.99@gmail.com>
2020-12-05 13:23:50 +0100

    Refactor get all books method in web book controller

--- src/main/java/es/urjc/cloudapps/library/presentation/web/BookController.java
+++ src/main/java/es/urjc/cloudapps/library/presentation/web/BookController.java
@@ -1,22 +1,28 @@
 package es.urjc.cloudapps.library.presentation.web;
 
 
+import es.urjc.cloudapps.library.application.BookService;
+import es.urjc.cloudapps.library.data.BookDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("/books")
-    public ModelAndView getAllBooks(Map<String, Object> model) {
-        System.out.println("ESTOY ENTRANDO AQUI");
+    public String getAllBooks(Map<String, Object> model) {
 
-        // ...
+        List<BookDto> books = this.bookService.findAll();
+        model.put("books", books);
 
-        return new ModelAndView("index", model);
+        return "index";
     }
 
 }
