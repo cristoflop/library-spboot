edc74e34b8b69c9fffb5ced94f716ee13dc94b40
cristoflop <cristofer.lc.99@gmail.com>
2020-12-05 18:12:48 +0100

    Optimize imports in book controller

--- src/main/java/es/urjc/cloudapps/library/presentation/web/BookController.java
+++ src/main/java/es/urjc/cloudapps/library/presentation/web/BookController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class BookController {
