ec62eef9da3c1aaeb1636b80f12e5e9f55374e85
cristoflop <cristofer.lc.99@gmail.com>
2020-12-07 13:22:55 +0100

    Refactor validation creating book moved to service

---
+++ src/main/java/es/urjc/cloudapps/library/exception/FieldFormatException.java
@@ -0,0 +1,9 @@
+package es.urjc.cloudapps.library.exception;
+
+public class FieldFormatException extends RuntimeException {
+
+    public FieldFormatException(String msg) {
+        super(msg);
+    }
+
+}
