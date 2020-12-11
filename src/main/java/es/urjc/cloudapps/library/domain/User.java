c7efd2291ef6803ebe9bc04c5a93273d05c68a5d
cristoflop <cristofer.lc.99@gmail.com>
2020-12-11 14:13:01 +0100

    Added entity user

---
+++ src/main/java/es/urjc/cloudapps/library/domain/User.java
@@ -0,0 +1,43 @@
+package es.urjc.cloudapps.library.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String nick;
+
+    private String email;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return this.nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
